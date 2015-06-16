6f486a292a14ac1b39b89ccbf08b9a97f3d4a58c
Benton <bentonpli@gmail.com>
2015-06-16 15:51:03 -0700

    updated job class
    changed it to float

--- src/FCFS.java
+++ src/FCFS.java
@@ -1,38 +1,27 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
-class FCFS {
-	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		int p;
-		float t1 = 0, t2 = 0;
-		System.out.println("Enter number of processes");
-		p = in.nextInt();
-		int bt[] = new int[p];
+public class FCFS {
 
-		System.out.println("Enter burst time");
-		for (int i = 0; i < p; i++) {
-			System.out.println("Burst time for P" + (i + 1) + "=");
-			bt[i] = in.nextInt();
+	public static ArrayList<Job> jobList = new ArrayList<Job>();
+	public static int[] arrivalArray = new int[20];
+
+	public static void main(String[] args) {
+		
+		// Fill the arrivalArray
+		for (int i = 0; i < 99; i++) {
+			arrivalArray[i] = i+1;
 		}
-		int wt[] = new int[p];
-		wt[0] = 0;
-		for (int i = 1; i < p; i++) {
-			wt[i] = bt[i - 1] + wt[i - 1];
-			t1 += wt[i];
+		// Add jobs to the jobList
+		for (int i = 0; i < 20; i++) {
+			Random r = new Random();
+			int jobNum = r.nextInt(100);
+			if (arrivalArray[jobNum] != 0) {
+				jobList.add(new Job("Job " + i, arrivalArray[jobNum], r.nextFloat()))
+			}
+			
+
 		}
-		int tat[] = new int[p];
-		for (int i = 0; i < p; i++) {
-			tat[i] = bt[i] + wt[i];
-			t2 += tat[i];
-		}
-		System.out
-				.println("Process\tBurst time\tWaiting time\tTurn Around time");
-		for (int i = 0; i < p; i++) {
-			System.out.println("P" + (i + 1) + "\t\t" + bt[i] + "\t\t" + wt[i]
-					+ "\t\t" + tat[i]);
-		}
-		float awt, atat;
-		System.out.println("Average Waiting time=" + t1 / p);
-		System.out.println("Average Turn Around time=" + t2 / p);
 	}
+
 }
--- src/Job.java
+++ src/Job.java
@@ -3,15 +3,15 @@ public class Job {
 	
 	// Necessary Job Variables
 	public String JobName;
-	public int ArrivalTime; // Time or Position it arrived
-	public int CPUTime; // Time it takes to complete
+	public float ArrivalTime; // Time or Position it arrived
+	public float CPUTime; // Time it takes to complete
 	public int Priority;
 	
 	// Extra Variables for Calculation
 	public int WaitingTime;
 	public int TurnAroundTime;
 	
-	public Job(String name, int arrival, int cpuTime, int priorityVal) {
+	public Job(String name, int arrival, float cpuTime, int priorityVal) {
 		this.JobName = name;
 		this.ArrivalTime = arrival;
 		this.CPUTime = cpuTime;
